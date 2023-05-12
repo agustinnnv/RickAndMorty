0c1c7de3fb2e604999bc5282085102741cac6c93
Agustin <aguvargass3@gmail.com>
2023-05-12 16:32:52 -0300

    creacion-de-api

--- Api/src/index.js
+++ Api/src/index.js
@@ -1,5 +1,5 @@
-const http = require('http')
-const characters = require("./Utils/data")
+const http = require('http');
+const characters = require("./Utils/data");
 
 const PORT = 3001
 http
@@ -10,12 +10,24 @@ http
     const param1 = url[1];
     const param2 = url[2];
     const id = url[3];
-    console.log(url)
+    // console.log(url)
     if(param1 === "rickandmorty" && param2 === "characters"){  // /rickandmorty/characters
-    }
+      res.writeHead(200, {"Content-type": "application/json" })
+     return res.end(JSON.stringify(characters));
+    };
     if(param1 === "rickandmorty" && param2 === "character"){  // /rickandmorty/character/:id
-    }
-    res.end("Hola rickosos");
+     // /rickandmorty/character/:id // 1 al 40
+     const character = characters.find((ch) => {
+        return ch.id === Number(id);
+     });
+     if (character)
+     return res
+     .writeHead(200, {"Content-type": "application/json" })
+     .end(JSON.stringify(character));
+     };
+    return res
+    .writeHead(404, {"Content-type": "text/plain" })
+    .end("Not found");
 }).listen(PORT, () => {
-    console.log("in port http://localhost:3001")
-})
+    console.log("in port http://localhost:3001");
+});
